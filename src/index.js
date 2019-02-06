4fa5bb0195be5e625caa243f193ed62c70a20cc1
Pablo <pablopst@gmail.com>
2019-02-06 19:38:17 -0200

    criado 2 cartões por js

--- src/index.js
+++ src/index.js
@@ -3,22 +3,38 @@
 const $root = document.querySelector("#root"); 
 
 //cria uma tag article e joga na variavel
-const $memoryCard = document.createElement("article");
+const $memoryCard      = document.createElement("article");
+const $memoryCardFront = document.createElement("article");
 //cria o html por texto e joga numa variavel
 //usando template string para adicionar o texto
-const $icon = `<img 
+const $iconCollab = `<img 
                     class='icon' 
                     src='img/icon-collabcode.png' 
                     alt='Gueio mascote da collabcode' 
-                />`
+                />`;
+
+const $iconC = `<img 
+                    class='icon' 
+                    src='img/icon-c.png' 
+                    alt='Gueio mascote da collabcode' 
+                />`;
+                
 //add uma classe na tag criada na variavel memoryCard
 $memoryCard.classList.add("memory-card");
+$root.insertBefore($memoryCard, null);
+
+
+
+$memoryCardFront.classList.add("memory-card");
+$memoryCardFront.classList.add("-front");
+$root.insertBefore($memoryCardFront, $memoryCard);
+
 
 //joga o texto transformando em html
 // $memoryCard.innerHTML = $icon;
 
-$root.insertBefore($memoryCard, null);
 
-$memoryCard.insertAdjacentHTML("afterbegin", $icon);
+$memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
+$memoryCardFront.insertAdjacentHTML("afterbegin", $iconC);
 
 
