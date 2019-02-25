0552725f3b046a0e7f0102eb13aba2859f58f58a
Pablo Teixeira <pablopst@gmail.com>
2019-02-25 11:47:53 -0300

    fim da aula 23

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,11 +1,7 @@
 
-//pega a rota para add os itens
-const $wrapCards = document.querySelector(".wrap-cards"); 
-
-
 function createMemoryCard(){
 
-    const $memoryCard      = document.createElement("article");
+    const $memoryCard = document.createElement("article");
 
 
     const $iconCollab = `<img 
@@ -14,9 +10,7 @@ function createMemoryCard(){
                         alt='Gueio mascote da collabcode' 
     />`;
 
-
-
-    $memoryCard.classList.add("memory-card");
+    $memoryCard.classList.add("memory-card"); 
     $wrapCards.insertBefore($memoryCard, null);
 
 
@@ -26,40 +20,33 @@ function createMemoryCard(){
 
 function createMemoryCardFront(){
     //cria uma tag article e joga na variavel
-// const $memoryCard      = document.createElement("article");
-const $memoryCardFront = document.createElement("article");
-//cria o html por texto e joga numa variavel
-//usando template string para adicionar o texto
-// const $iconCollab = `<img 
-//                     class='icon' 
-//                     src='img/icon-collabcode.png' 
-//                     alt='Gueio mascote da collabcode' 
-//                 />`; 
+    // const $memoryCard      = document.createElement("article");
+    const $memoryCardFront = document.createElement("article");
 
 
 
-const $iconC = `<img 
-                    class='icon' 
-                    src='img/icon-c.png' 
-                    alt='icone de um livro de c++' 
-                />`;
-                
-//add uma classe na tag criada na variavel memoryCard
-// $memoryCard.classList.add("memory-card");
-// $root.insertBefore($memoryCard, null);
+    const $iconC = `<img 
+                        class='icon' 
+                        src='img/icon-c.png' 
+                        alt='icone de um livro de c++' 
+                    />`;
+                    
+    //add uma classe na tag criada na variavel memoryCard
+    // $memoryCard.classList.add("memory-card");
+    // $root.insertBefore($memoryCard, null);
 
 
 
-$memoryCardFront.classList.add("memory-card");
-$memoryCardFront.classList.add("-front");
-$wrapCards.insertBefore($memoryCardFront, null);
+    $memoryCardFront.classList.add("memory-card");
+    $memoryCardFront.classList.add("-front");
+    $wrapCards.insertBefore($memoryCardFront, null);
 
 
-//joga o texto transformando em html
-// $memoryCard.innerHTML = $icon;
+    //joga o texto transformando em html
+    // $memoryCard.innerHTML = $icon;
 
 
-// $memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
-$memoryCardFront.insertAdjacentHTML("afterbegin", $iconC);
+    // $memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
+    $memoryCardFront.insertAdjacentHTML("afterbegin", $iconC);
 
 }
